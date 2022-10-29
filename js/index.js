05527167caee307e8aecd6c4ed5571324b1faeb8
Diego Neves <pdcassiano@gmail.com>
2022-10-29 13:15:09 -0300

    chore: add mudar de unidade - ainda não funcionou

--- js/index.js
+++ js/index.js
@@ -1,53 +1,47 @@
-document.getElementById("border-radius-1").value;
+document.getElementById('border-radius-1').value
 
 function mundarBorderRadius() {
-  const square = document.getElementById("square");
-  const codeCss = document.getElementById("css");
-  const valueLeftTop = document.getElementById("border-radius-top-left-value");
-  const valueRightTop = document.getElementById(
-    "border-radius-top-right-value"
-  );
+  const square = document.getElementById('square')
+  const codeCss = document.getElementById('css')
+  const valueLeftTop = document.getElementById('border-radius-top-left-value')
+  const valueRightTop = document.getElementById('border-radius-top-right-value')
   const valueRightBotton = document.getElementById(
-    "border-radius-botton-right-value"
-  );
+    'border-radius-botton-right-value'
+  )
   const valueLeftBotton = document.getElementById(
-    "border-radius-botton-left-value"
-  );
+    'border-radius-botton-left-value'
+  )
 
   let border_radius_top_left =
-    document.getElementById("border-radius-top-left").value + "%";
+    document.getElementById('border-radius-top-left').value +
+    document.querySelector('input[type="radio"]:checked').value
   let border_radius_top_right =
-    document.getElementById("border-radius-top-right").value + "%";
+    document.getElementById('border-radius-top-right').value +
+    document.querySelector('input[type="radio"]:checked').value
   let border_radius_botton_right =
-    document.getElementById("border-radius-botton-right").value + "%";
+    document.getElementById('border-radius-botton-right').value +
+    document.querySelector('input[type="radio"]:checked').value
   let border_radius_botton_left =
-    document.getElementById("border-radius-botton-left").value + "%";
+    document.getElementById('border-radius-botton-left').value +
+    document.querySelector('input[type="radio"]:checked').value
   square.style.borderRadius =
     border_radius_top_left +
     border_radius_top_right +
     border_radius_botton_right +
-    border_radius_botton_left;
+    border_radius_botton_left
 
-  valueLeftTop.textContent = border_radius_top_left;
-  valueRightTop.textContent = border_radius_top_right;
-  valueRightBotton.textContent = border_radius_botton_right;
-  valueLeftBotton.textContent = border_radius_botton_left;
+  valueLeftTop.textContent = border_radius_top_left
+  valueRightTop.textContent = border_radius_top_right
+  valueRightBotton.textContent = border_radius_botton_right
+  valueLeftBotton.textContent = border_radius_botton_left
   codeCss.value =
-    "border-radius: " +
-    border_radius_top_left +
-    " " +
-    border_radius_top_right +
-    " " +
-    border_radius_botton_right +
-    " " +
-    border_radius_botton_left;
-  +";";
+    `border-radius: ${border_radius_top_left} ${border_radius_top_right} ${border_radius_botton_right} ${border_radius_botton_left};`
 }
 
 function copiarCodigo() {
-  const codeCss = document.getElementById("css");
-  codeCss.select();
-  codeCss.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  alert("Copiado para a área de transferência");
+  const codeCss = document.getElementById('css')
+  codeCss.select()
+  codeCss.setSelectionRange(0, 99999)
+  document.execCommand('copy')
+  alert('Copiado para a área de transferência')
 }
